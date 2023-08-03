dde1f1eb9c84b4481ae9ba1cfe658f2e65c905d1
adeebaimam <123718308+adeebaimam@users.noreply.github.com>
2023-08-03 14:22:16 +0530

    Create Q8.py

---
+++ module1/Q8.py
@@ -0,0 +1,4 @@
+#Exercise 7: Return the count of a given substring from a string
+mystr = "emma is a good writer,emma is a good developer too"
+x = mystr.count("emma")
+print("emma apperared ", x ," times")
