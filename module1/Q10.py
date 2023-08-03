4e7edc9087cabf6477eb5a7a678c32003363efbd
adeebaimam <123718308+adeebaimam@users.noreply.github.com>
2023-08-03 14:39:45 +0530

    Update Q10.py

--- module1/Q10.py
+++ module1/Q10.py
@@ -4,15 +4,17 @@
 n = int(input("enter a no. "))
 print( "actual_no. = ", n)
 
+original_no = n
 reverse = 0    
+
 while n > 0:
     digit = n%10
     reverse = reverse*10 + digit           
     n = n//10 
 print("reversed no. = ", reverse )
     
-if reverse == n:
-    print(" yes, it is a palindrome no")
+if reverse == original_no :
+    print("yes, it is a palindrome no")
 else:
-    print( "no, it is not a plainfrome no.")
+    print( "no, it is not a palindrome no.")
         
