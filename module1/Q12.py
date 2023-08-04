cc5aaa7ca740ef096782f52429d4cfe3d413f195
adeebaimam <123718308+adeebaimam@users.noreply.github.com>
2023-08-04 09:59:42 +0530

    Create Q12.py

---
+++ module1/Q12.py
@@ -0,0 +1,8 @@
+#Exercise 13: Print multiplication table form 1 to 10
+
+for i in range(1,11):
+    for j in range(1,11):
+        # to show multiplication in a single line we use end following with space 
+        print(i*j,end =' ' )
+    print("\n")    
+        
