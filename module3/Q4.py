06af566dd809813618c3cbf0d8963230b1b7ca5e
adeebaimam <123718308+adeebaimam@users.noreply.github.com>
2023-08-08 21:25:59 +0530

    Create Q4.py

---
+++ module3/Q4.py
@@ -0,0 +1,10 @@
+#Exercise 5: Accept a list of 5 float numbers as an input from the user:
+num = []
+for i in range(0,5):
+    print("enter numbers")
+    inputs = float(input())
+    num.append(inputs)
+    
+
+print(num)
+             
