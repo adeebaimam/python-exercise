7ad1a17c6e72e2ee1988fd20c59efb1d7ba26e2a
adeebaimam <123718308+adeebaimam@users.noreply.github.com>
2023-08-12 22:30:57 +0530

    Create Q2.py

---
+++ module4/Q2.py
@@ -0,0 +1,7 @@
+#program to create function calculation() such that it can accept two variables and calculate addition and subtraction and it must return both in a single return call.
+def calculate(a,b):
+    add = a+b
+    sub = a-b
+    return add,sub
+print(calculate(40,10))
+    
