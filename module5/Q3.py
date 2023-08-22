50ed32fb146b74bc081a11c762f6d81dbc9601db
adeebaimam <123718308+adeebaimam@users.noreply.github.com>
2023-08-22 21:10:50 +0530

    Update Q3.py

--- module5/Q3.py
+++ module5/Q3.py
@@ -1,12 +1,9 @@
 #Count the occurrence of each element from a list
 sample_list = [11, 45, 8, 11, 23, 45, 23, 45, 89]
-print("Original list ", sample_list)
-
-count_dict = dict()
-for item in sample_list:
-    if item in count_dict:
-        count_dict[item] += 1
-    else:
-        count_dict[item] = 1
-
-print("Printing count of each item  ", count_dict)
+count={}
+for i in sample_list:
+    if i in count:
+        count[i]+=1
+    else :
+        count[i]=1
+print(count)
