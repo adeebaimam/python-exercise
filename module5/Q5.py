dcd777d5274a1b72e60a4fc1e7b592ad9b57d656
adeebaimam <123718308+adeebaimam@users.noreply.github.com>
2023-08-23 23:53:47 +0530

    Create Q5.py

---
+++ module5/Q5.py
@@ -0,0 +1,9 @@
+#Convert string into a datetime object
+from datetime import datetime
+date_string = "Feb 25 2020 4:20PM"
+
+#to convert string into date time object .sprtime funcn is used
+#%b-month name,%d-date of a month,%Y-year,%I-hour(01-12),%p-am,pm
+
+datetime_object=datetime.strptime(date_string,"%b %d %Y %I:%M%p")
+print(datetime_object)
