add6284702a204447494c8427ab0f5544f844cf7
adeebaimam <123718308+adeebaimam@users.noreply.github.com>
2023-08-25 00:51:12 +0530

    Create Q6.py

---
+++ Q6.py
@@ -0,0 +1,7 @@
+#Write a program that will tell whether the number entered by the user is odd or even.
+n = int(input("Please enter a number"))
+if n%2==0:
+    print("It is an even number")
+else:
+    print("It is a odd number")
+
