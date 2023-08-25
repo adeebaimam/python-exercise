126533c3ae260023c60d2e4773bcd1877233e972
adeebaimam <123718308+adeebaimam@users.noreply.github.com>
2023-08-25 14:33:54 +0530

    Create Q11.py

---
+++ Q11.py
@@ -0,0 +1,6 @@
+#Write a program to find the simple interest when the value of principle,rate of interest and time period is given.
+principle = P = 2700
+rate_of_percent = R = 3
+time_period = T = 2 #2years
+SI = (P*R*T)/100
+print("Simple Interest = ",SI)
