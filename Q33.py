3693a702efdba20a9a4e66d9b78e34f24a8880f0
adeebaimam <123718308+adeebaimam@users.noreply.github.com>
2023-09-01 12:12:41 +0530

    Create Q33.py

---
+++ Q33.py
@@ -0,0 +1,10 @@
+#The natural logarithm can be approximated by the following series.
+#((x-1)/x)+1/2((x-1)/x)^2+1/2((x-1)/x)^3+...
+#If x is input through the keyboard, write a program to calculate the sum of the first seven terms of this series.
+x = int(input("Enter the value of x: "))
+n = int(input("Enter the number of terms: "))
+result = 1-1/x
+for i in range(2,n+1):
+    series = pow(((x-1)/x),i)
+    result = result + (series/2)
+print(result)
