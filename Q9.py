fcb70f89413d3b88d4be5ee3a020b74ee89892ef
adeebaimam <123718308+adeebaimam@users.noreply.github.com>
2023-09-17 22:46:35 +0530

    Update Q9.py

--- Q9.py
+++ Q9.py
@@ -1,4 +1,4 @@
-#Write a program that take a user inputr of three angles and will find out whether it can form a triangle or not.
+# Write a program that take a user inputr of three angles and will find out whether it can form a triangle or not.
 n = input("enter three angles: " )
 x,y,z = map(int,n.split())
 if x+y+z==180:
