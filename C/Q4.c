fe78ff98881b36f945c2294b315127d39732edb0
adeebaimam <123718308+adeebaimam@users.noreply.github.com>
2023-10-14 20:37:14 +0530

    Update Q4.c

--- C/Q4.c
+++ C/Q4.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <conio,h>
+#include <conio.h>
 #include <math.h>
 
 int main() {
@@ -9,6 +9,6 @@ int main() {
     scanf("%f%f",&x,&y);
     r=sqrt(x*x+y*y);
     theta=atan(y/x);
-    printf("r=%.2f,theta=%.2f",r,theta);
+    printf("Converting cartesian co-ordinates into polar co-ordinates =(%.2f ,%.2f)",r,theta);
     return 0;
 }
